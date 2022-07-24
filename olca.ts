a1de068fbb131cfccc4bb9ed768e9b3c24f63097
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-07-24 19:36:22 +0900

    更新 olca.ts

--- olca.ts
+++ olca.ts
@@ -190,6 +190,27 @@ else {
     //    basic.showString("V1");
 }
 
+basic.forever(function () {
+    let moving = 0
+    serial.writeLine("" + (moving))
+    if (moving == 0) {
+        olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.forward, 0)
+    }
+    if (moving == 1) {
+        olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.forward, 1)
+    }
+    if (moving == 2) {
+        olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.backward, 1)
+    }
+    if (moving == 3) {
+        olca_plot_car.plot_RL_cycle(olca_plot_car.plotter_RL.Right, 1)
+    }
+    if (moving == 4) {
+        olca_plot_car.plot_RL_cycle(olca_plot_car.plotter_RL.left, 1)
+    }
+})
+
+
 
 //% color="#3943c6" block="Olca Ver1.0" weight=95 icon="\uf1b9"
 namespace olca_plot_car {
