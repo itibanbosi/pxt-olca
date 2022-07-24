b74f18bb0d05e768c48b0e042864096c6cee9323
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-07-24 20:01:14 +0900

    更新 olca.ts

--- olca.ts
+++ olca.ts
@@ -279,27 +279,6 @@ namespace olca_plot_car {
         white
     }
 
-    basic.forever(function () {
-        let moving = 0
-        serial.writeLine("" + (moving))
-        if (moving == 0) {
-            olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.forward, 0)
-        }
-        if (moving == 1) {
-            olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.forward, 1)
-        }
-        if (moving == 2) {
-            olca_plot_car.plot_zengo(olca_plot_car.plotter_houkou.backward, 1)
-        }
-        if (moving == 3) {
-            olca_plot_car.plot_RL_cycle(olca_plot_car.plotter_RL.Right, 1)
-        }
-        if (moving == 4) {
-            olca_plot_car.plot_RL_cycle(olca_plot_car.plotter_RL.left, 1)
-        }
-    })
-
-
 
     function moter(kyori: number, R_zengo: number, L_zengo: number) {
         led.enable(false);
